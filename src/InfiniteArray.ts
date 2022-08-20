8c87cf3ac46c17f9075cb75eeff41a608dcca591
Carlos Zanella <cefzanella@gmail.com>
2022-08-20 18:39:16 -0300

    feat: adds InfiniteArray

---
+++ src/InfiniteArray.ts
@@ -0,0 +1,27 @@
+export class InfiniteArray<T> {
+  private cache: T[];
+  private startRange: number;
+  private endRange: number;
+  private defaultValue: T;
+
+  constructor(startRange: number, endRange: number, defaultValue: T) {
+    this.cache = new Array<T>(endRange - startRange + 1);
+    this.startRange = startRange;
+    this.endRange = endRange;
+    this.defaultValue = defaultValue;
+  }
+
+  set(index: number, value: T) {
+    if (index >= this.startRange && index <= this.endRange) {
+      this.cache[index - this.startRange] = value;
+    }
+  }
+
+  get(index: number) {
+    if (index >= this.startRange && index <= this.endRange) {
+      return this.cache[index - this.startRange];
+    }
+
+    return this.defaultValue;
+  }
+}
